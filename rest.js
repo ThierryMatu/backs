edad9ce8994d4432c701ada9fb3c7f5dd2f73d8e
ThierryMatu <101680040+ThierryMatu@users.noreply.github.com>
2022-06-15 01:58:48 +0100

    Update rest.js

--- rest.js
+++ rest.js
@@ -1,101 +1,103 @@
-const express = require("express")
-const cors = require("cors")
-const path = require("path")
-const fs = require("fs")
-const http = require("http")
+const path = require('path')
+const fs = require('fs')
+const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const cors = require('cors')
 
-// MIDDLEWARES
-// use Parser Middleware
 app.use(express.json())
-
-// use Logger middleware
-app.use(function (req, res, next) {
-  console.log('Request IP: ' + req.url)
-  console.log('Request date: ' + new Date())
-  return next()
-})
-
-// use param middleware
-app.param('collectionName', function (req, res, next, collectionName) {
-  req.collection = db.collection(collectionName)
-  return next()
-})
-
-// use static file middleware
-app.use(function (req, res, next) {
-  const filePath = path.join(__dirname, 'static', req.url)
-  fs.stat(filePath, function (err, fileInfo) {
-    if (err) return next()
-    if (fileInfo.isFile()) res.sendFile(filePath)
-    else next()
-  })
-})
-
-// CORS allows you to configure the web API's security. 
-// It has to do with allowing other domains to make requests against your web API.
 app.use(cors());
 
-
-// connect to MongoDB Database
+// MONGODB CONNECTION
 const MongoClient = require('mongodb').MongoClient;
 let db;
-MongoClient.connect('mongodb+srv://Enoch:Matu@cluster0.4b3r7.mongodb.net/test', (err, client) => {
+MongoClient.connect("mongodb+srv://Enoch:Matu@cluster0.4b3r7.mongodb.net/test", (err, client) => {
     db = client.db('afterschool')
 })
+        
+// GET THE COLLECTION NAME
+app.param('collectionName', (req, res, next, collectionName) => {
+    req.collection = db.collection(collectionName)
+    return next()
+})
 
+// MIDDLEWARE FOR LOGGING
+app.use(function(req, res, next){
+    console.log("Request type: "+req.method)
+    console.log("Request url: "+req.url)
+    console.log("Request date: "+new Date())
+    console.log("Request IP: "+req.ip)
+    next()
+})
 
 app.get('/', (req, res) => {
-  res.send("Welcome to entry point")
+    res.send("Welcome to entry point")
 })
 
-
-
-// Endpoint to get all lessons
-
+// GET ALL LESSONS FOM DB
 app.get('/collection/:collectionName', (req, res) => {
-  req.collection.find({}).toArray((err, results) => {
-      if (err) return next(err)
-      res.send(results)
-  })
+    req.collection.find({}).toArray((err, results) => {
+        if (err) return next(err)
+        res.send(results)
+    })
 })
-
-
-
-// Endpoint to add an order
+//Add order to db
 app.post('/collection/:collectionName', (req, res) => {
-  let doc = req.body
-  req.collection.insertOne(doc, (err, result) => {
-      if (err) return next(err)
-      res.send({msg: "order added successfully"})
-  })
+    let doc = req.body
+    req.collection.insertOne(doc, (err, result) => {
+        if (err) return next(err)
+        res.send({msg: "order added successfully"})
+    })
 })
 
-// Endpoint to update number of available spaces in lesson
-app.put('/collection/:collectionName/:id', async function (req, res, next) {
-  await req.collection.updateOne(
-    { _id: new ObjectId(req.params.id) },
-    { $set: { space: req.body.space } },
-    (e, results) => {
-      res.json(results)
-      if (e) return next(e)
-  })
+// UPDATE SPACES OF LESSONS IN DB AFTER ORDER
+app.put('/collection/:collectionName', (req, res) => {
+    req.body.forEach((item) => {
+        let filter = { id: item.id }
+        let new_value = { $set: {space: item.space} }
+        let options = { safe: true, multi: false }
+        req.collection.updateOne(filter, new_value, options, (err, result) => {
+            if (err) return next(err)
+        })
+    });
+    res.send({msg: "spaces successfully updated"})
 })
 
-// Endpoint to perform a Full Text Search on lessons
+
+// BACKEND SEARCH FUNCTION
 app.get('/collection/:collectionName/search', (req, res) => {
-  let search_keyword = req.query.search
-  req.collection.find({}).toArray((err, results) => {
-      if (err) return next(err)
-      let filteredList = results.filter((subject) => {
-          return subject.subjectname.toLowerCase().match(search_keyword.toLowerCase()) || subject.location.toLowerCase().match(search_keyword.toLowerCase())
-      });  
-      res.send(filteredList)
-  })
+    let search_keyword = req.query.search
+    req.collection.find({}).toArray((err, results) => {
+        if (err) return next(err)
+        let filteredList = results.filter((subject) => {
+            return subject.topic.toLowerCase().match(search_keyword.toLowerCase()) || subject.location.toLowerCase().match(search_keyword.toLowerCase())
+        });  
+        res.send(filteredList)
+    })
 })
 
-// Listen to port
-app.listen(port, function () {
-  console.log(`Server running at http://localhost:${port}`);
-});
+// STATIC FILE MIDDLEWARE
+app.use(function(req, res, next){
+    var filePath = path.join(__dirname, "static", req.url)
+    fs.stat(filePath, function(err, fileInfo){
+        if (err) {
+            next()
+            return
+        }
+        if (fileInfo.isFile()) {
+            res.sendFile(filePath)
+        }
+        else{
+            next()
+        }
+    })
+})
+
+app.use(function(req, res){
+    res.status(404)
+    res.send("file not found")
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log("Running on port 3000")
+})
